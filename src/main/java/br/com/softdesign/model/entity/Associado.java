15c182547b8d7f825e0158745d8fc5ca7b085235
Luiz Junior <lsjunior.it@gmail.com>
2021-02-10 20:16:22 -0300

    Impl da classe Associado

--- src/main/java/br/com/softdesign/model/entity/Associado.java
+++ src/main/java/br/com/softdesign/model/entity/Associado.java
@@ -1,4 +1,38 @@
 package br.com.softdesign.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+/**
+ * @author Junior
+ * @since 10/02/2021
+ * @version 1.0
+ *
+ * Classe criada para implementar um objeto Associado
+ */
+
+/*Anotação para realcionar a classe Associado a entidade do banco de dados*/
+@Entity
+/*Anotações Lombok para gerar os construtores em tempo de execução*/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Associado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column
+    private LocalDate dataCadstro;
 }
