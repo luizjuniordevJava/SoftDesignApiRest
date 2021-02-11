b8c5a7ba95e847cb1b054258c7e86a115445014c
Luiz Junior <lsjunior.it@gmail.com>
2021-02-10 23:50:11 -0300

    implementação do repositório de associados

---
+++ src/main/java/br/com/softdesign/model/repository/AssociadoRepository.java
@@ -0,0 +1,7 @@
+package br.com.softdesign.model.repository;
+
+import br.com.softdesign.model.entity.Associado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssociadoRepository extends JpaRepository<Associado,Integer> {
+}
