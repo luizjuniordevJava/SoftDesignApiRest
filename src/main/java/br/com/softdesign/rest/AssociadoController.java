be1f9b538fac26c19c73add7ccbfe6990197c8cb
Luiz Junior <lsjunior.it@gmail.com>
2021-02-11 13:33:03 -0300

    Implementação do método achar por id AssociadoController.java Teste de buscado através do Postman

--- src/main/java/br/com/softdesign/rest/AssociadoController.java
+++ src/main/java/br/com/softdesign/rest/AssociadoController.java
@@ -5,6 +5,7 @@ import br.com.softdesign.model.repository.AssociadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  * @author Junior
@@ -21,17 +22,28 @@ public class AssociadoController {
 
     private final AssociadoRepository repository;
 
+    /*Anotação para permitir a injeção do repository*/
     @Autowired
     public AssociadoController(AssociadoRepository repository){
+
         this.repository = repository;
     }
 
-
+    /*Anotação para informar que metodo é um post*/
     @PostMapping
-    /*Anotação para ter o retorno da requisição*/
+    /*Anotação para ter o retorno da requisição para o client*/
     @ResponseStatus(HttpStatus.CREATED)
-    public Associado salvar( @RequestBody //Anotação para indicar que o objeto será passado no corpo da requisição
-                    Associado associado ){
+    public Associado salvar( @RequestBody Associado associado ){
+        /*Anotação RequestBody indica que o objeto JSON será passado no corpo da requisição*/
         return repository.save(associado);
+    }
+
+    /*Anotação para pegar o parâmeto id passado na url*/
+    @GetMapping("{id}")
+    public Associado acharPorId( @PathVariable Integer id){
+        return repository
+                .findById(id)
+                .orElseThrow(()-> new ResponseStatusException((HttpStatus.NOT_FOUND)));
+                /*Se não encontrar o cliente por id será lançado o erro que objeto não foi encontrado*/
     }
 }
