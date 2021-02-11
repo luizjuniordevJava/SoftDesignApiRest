bf4b585ea9a91de1fa8c97c42e15d0841a064f5d
Luiz Junior <lsjunior.it@gmail.com>
2021-02-10 23:30:18 -0300

    Ajustes imports

--- src/main/java/br/com/softdesign/model/entity/Pauta.java
+++ src/main/java/br/com/softdesign/model/entity/Pauta.java
@@ -3,7 +3,6 @@ package br.com.softdesign.model.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 
 /**
--- src/main/java/br/com/softdesign/model/entity/Voto.java
+++ src/main/java/br/com/softdesign/model/entity/Voto.java
@@ -4,34 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
-import java.time.LocalDate;
 
 /**
  * @author Junior
  * @since 10/02/2021
  * @version 1.0
  *
- * Classe criada para implementar um objeto Associado
+ * Classe criada para implementar os Votos da Pauta
  */
-
 /*Anotações Lombok para gerar os construtores, getter e setter em tempo de execução*/
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-/*Anotação para relacionar a classe Associado a entidade do banco de dados*/
+/*Anotação para relacionar a classe Voto a entidade do banco de dados*/
 @Entity
-public class Associado {
-
+public class Voto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false, length = 150)
-    private String nome;
-
-    @Column(nullable = false, length = 11)
-    private String cpf;
-
     @Column
-    private LocalDate dataCadstro;
+    private Boolean voto;
+
+    /*Anotação para relacionar o Voto ao Associado*/
+    @OneToOne
+    @JoinColumn(name="id_associado")
+    private Associado associado;
 }
