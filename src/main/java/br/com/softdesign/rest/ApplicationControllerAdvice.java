0b8d7e02ced88932c06ea12a4f9a481648f13d85
Luiz Junior <lsjunior.it@gmail.com>
2021-02-11 17:53:06 -0300

    Implementação da classe ApplicationControllerAdvice.java para interceptar os erros e tratar os dados

---
+++ src/main/java/br/com/softdesign/rest/ApplicationControllerAdvice.java
@@ -0,0 +1,35 @@
+package br.com.softdesign.rest;
+
+import br.com.softdesign.rest.exception.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Junior
+ * @since 11/02/2021
+ * @version 1.0
+ *
+ * Classe criada para interceptar os erros e tratar os dados
+ */
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Object handleValidationErrors(MethodArgumentNotValidException ex){
+        BindingResult bindingResult = ex.getBindingResult();
+        List<String> messages = bindingResult.getAllErrors()
+                                    .stream()
+                                    .map( objectError -> objectError.getDefaultMessage())
+                                    .collect(Collectors.toList());
+        return new ApiErrors((messages));/* retorno do objeto padronizado*/
+    }
+}
