edf687bdc421585548935ec5d320ad0574fcf520
Luiz Junior <lsjunior.it@gmail.com>
2021-02-10 23:06:08 -0300

    Implementação da classe Pauta.java

---
+++ src/main/java/br/com/softdesign/model/entity/Pauta.java
@@ -0,0 +1,31 @@
+package br.com.softdesign.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * @author Junior
+ * @since 10/02/2021
+ * @version 1.0
+ *
+ * Classe criada para implementar um objeto Pauta
+ */
+
+/*Anotação para relacionar a classe Pauta a entidade do banco de dados*/
+@Entity
+/*Anotações Lombok para gerar os construtores, getter e setter em tempo de execução*/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pauta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 250)
+    private String descricao;
+}
