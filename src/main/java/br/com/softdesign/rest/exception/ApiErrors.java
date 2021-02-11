2c8e5c2203416b01004708752e33bd26fac69ce2
Luiz Junior <lsjunior.it@gmail.com>
2021-02-11 17:51:41 -0300

    Implementação da classe ApiErrors.java para padronizar os erros em objeto JSON

---
+++ src/main/java/br/com/softdesign/rest/exception/ApiErrors.java
@@ -0,0 +1,29 @@
+package br.com.softdesign.rest.exception;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Junior
+ * @since 11/02/2021
+ * @version 1.0
+ *
+ * Classe criada para padronizar os erros em objeto JSON
+ */
+
+public class ApiErrors {
+
+    /*Anotação Lombok para gerar o metodo get em tempo execução*/
+    @Getter
+    private List<String> errors;
+
+    public ApiErrors(List<String> errors){
+        this.errors = errors;
+    }
+
+    public ApiErrors(String message){
+        this.errors = Arrays.asList(message);
+    }
+}
