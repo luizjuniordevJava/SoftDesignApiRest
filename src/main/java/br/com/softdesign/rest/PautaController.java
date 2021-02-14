860dba843101c26bf23b1ca2211038915e290a8e
Luiz Junior <lsjunior.it@gmail.com>
2021-02-13 22:47:42 -0300

    Implementação do CORS na pauta controller

--- src/main/java/br/com/softdesign/rest/PautaController.java
+++ src/main/java/br/com/softdesign/rest/PautaController.java
@@ -22,6 +22,7 @@ import java.util.List;
 @RestController
 /*Anotação para indicar a rota*/
 @RequestMapping("/api/pautas")
+@CrossOrigin("http://localhost:4200")
 public class PautaController {
 
     private final PautaRepository repository;
