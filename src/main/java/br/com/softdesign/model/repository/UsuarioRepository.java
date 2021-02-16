7d0bcb41229b31e2c8adbf9c4904efb01b23ba93
Luiz Junior <lsjunior.it@gmail.com>
2021-02-16 13:30:07 -0300

    Implementação da interface UsuarioRepository

---
+++ src/main/java/br/com/softdesign/model/repository/UsuarioRepository.java
@@ -0,0 +1,17 @@
+package br.com.softdesign.model.repository;
+
+import br.com.softdesign.model.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author Junior
+ * @since 16/02/2021
+ * @version 1.0
+ *
+ * Interface criada para extender a classe pai JpaRepository
+ * e ter acesso aos metodos do CRUD
+ *
+ */
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+}
