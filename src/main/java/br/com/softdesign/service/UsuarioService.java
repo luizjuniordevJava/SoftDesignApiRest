cc9cff7cdd39f8574bb5689d9d44a216d702d0e2
Luiz Junior <lsjunior.it@gmail.com>
2021-02-16 19:47:05 -0300

    Implementação da classe UsuarioService.java para ser utilizado como serviço tanto no login quanto no spring SecurityConfig

---
+++ src/main/java/br/com/softdesign/service/UsuarioService.java
@@ -0,0 +1,42 @@
+package br.com.softdesign.service;
+
+import br.com.softdesign.model.entity.Usuario;
+import br.com.softdesign.model.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Junior
+ * @since 16/02/2021
+ * @version 1.0
+ *
+ * Classe criada para implementar UserDetailsService e ter acesso ao metodo
+ * loadUserByUsername e ser utilizado tanto no login quanto no spring security
+ *
+ *
+ */
+
+@Service
+public class UsuarioService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = repository
+                            .findByUsername(username)
+                            .orElseThrow(()-> new UsernameNotFoundException("Login não encontrado!"));
+
+        return User
+                    .builder()
+                    .username(usuario.getUsername())
+                    .password(usuario.getPassword())
+                    .roles("USER")
+                    .build();
+    }
+}
