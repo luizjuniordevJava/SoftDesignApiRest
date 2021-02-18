b65d032571d12bdc28b1823786848c5e0d331cc8
Luiz Junior <lsjunior.it@gmail.com>
2021-02-18 17:13:24 -0300

    Refatoração da PautaController.java para conter somente os metodos para o client

--- src/main/java/br/com/softdesign/rest/PautaController.java
+++ src/main/java/br/com/softdesign/rest/PautaController.java
@@ -1,8 +1,10 @@
 package br.com.softdesign.rest;
 
+import br.com.softdesign.exception.PautaNaoEncontradaException;
 import br.com.softdesign.model.entity.Pauta;
-import br.com.softdesign.model.repository.PautaRepository;
+import br.com.softdesign.service.PautaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -31,73 +33,70 @@ public class PautaController {
     /**
      * Anotação Autowired para permitir a injeção do repository
      */
-    private final PautaRepository repository;
+    @Autowired
+    private final PautaService service;
 
     @GetMapping
     public List<Pauta> obterTodos(){
-
-        return repository.findAll();
+        return service.listarTodos();
     }
 
     /**
-     *  Anotação para informar que metodo é um post
-     *  Anotação para ter o retorno da requisição para o client
+     *  Anotação PostMapping para informar que metodo é um post
+     *  Anotação ResponseStatus(HttpStatus.CREATED) retorno de msg pro client
      *  Anotação RequestBody indica que o objeto JSON será passado no corpo da requisição
      *  Anotação Valid para validar o objeto pauta
-     *  Anotação ResponseStatus(HttpStatus.CREATED) retorno de msg pro client
      *
      */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Pauta salvar( @RequestBody @Valid Pauta pauta ){
-        return repository.save(pauta);
+        try {
+            return service.salvar( pauta );
+        }catch (PautaNaoEncontradaException e){
+            throw new ResponseStatusException( HttpStatus.NOT_FOUND, e.getMessage() );
+        }
+
     }
 
     /**
      *  Anotação GetMapping para pegar o parâmeto passado na url
-     *  findById primeiro busca a pauta
-     *  orElseThrow se não encontrar a pauta por id será lançado o erro que objeto não foi encontrado
      */
     @GetMapping("{id}")
     public Pauta acharPorId( @PathVariable Integer id){
-        return repository
-                .findById(id)
-                .orElseThrow( ()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"pauta não encontrado!"));
+        try {
+            return service.acharPorId(id);
+        }catch (PautaNaoEncontradaException e){
+            throw new ResponseStatusException( HttpStatus.NOT_FOUND, e.getMessage() );
+        }
     }
 
     /**
      *  Anotação DeleteMapping para pegar o parâmeto passado na url, informar que metodo é um Delete
      *  Anotação ResponseStatus(HttpStatus.NO_CONTENT) para ter o retorno 204 sucesso no NO_CONTENT
-     *  findById primeiro busca a pauta
-     *  orElseThrow se não encontrar a pauta por id será lançado o erro que objeto não foi encontrado
      */
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletar( @PathVariable Integer id){
-        repository
-                .findById(id)
-                .map( pauta -> {
-                    repository.delete(pauta);
-                    return Void.TYPE;
-                })
-                .orElseThrow( ()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"pauta não encontrado!"));
+        try {
+            service.deletar(id);
+        }catch (PautaNaoEncontradaException e){
+            throw new ResponseStatusException( HttpStatus.NOT_FOUND, e.getMessage() );
+        }
     }
 
     /**
      *  Anotação DeleteMapping para pegar o parâmeto passado na url, informar que metodo é um Delete
      *  Anotação ResponseStatus(HttpStatus.NO_CONTENT) para ter o retorno 204 sucesso no NO_CONTENT
-     *  findById primeiro busca a pauta
-     *  orElseThrow se não encontrar a pauta por id será lançado o erro que objeto não foi encontrado
      */
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void atualizar(@PathVariable Integer id, @RequestBody @Valid Pauta pautaAtualizado){
-        repository
-                .findById(id)
-                .map( pauta -> {
-                    pautaAtualizado.setId(pauta.getId());
-                    return repository.save(pautaAtualizado);
-                })
-                .orElseThrow( ()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"pauta não encontrado!"));
+        try {
+            service.atualizar( id, pautaAtualizado );
+        }catch (PautaNaoEncontradaException e){
+            throw new ResponseStatusException( HttpStatus.NOT_FOUND, e.getMessage() );
+        }
+
     }
 }
