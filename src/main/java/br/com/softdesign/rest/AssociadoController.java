b658b13694e16e56ef1c8eca91837c52ed2921ef
Luiz Junior <lsjunior.it@gmail.com>
2021-02-18 16:52:02 -0300

    AssociadoController.java ajustada para conter somente os metodos referente ao client

--- src/main/java/br/com/softdesign/rest/AssociadoController.java
+++ src/main/java/br/com/softdesign/rest/AssociadoController.java
@@ -1,7 +1,9 @@
 package br.com.softdesign.rest;
 
+import br.com.softdesign.exception.AssociadoNaoEncontadoException;
 import br.com.softdesign.model.entity.Associado;
 import br.com.softdesign.model.repository.AssociadoRepository;
+import br.com.softdesign.service.AssociadoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,77 +31,78 @@ import java.util.List;
 public class AssociadoController {
 
     /**
-     * Anotação Autowired para permitir a injeção do repository
+     * Anotação Autowired para permitir a injeção do AssociadoService
      */
     @Autowired
     private final AssociadoRepository repository;
 
+    @Autowired
+    private final AssociadoService service;
+
     @GetMapping
     public List<Associado> obterTodos(){
-        return repository.findAll();
+        return service.listarTodos();
     }
 
     /**
-     *  Anotação para informar que metodo é um post
-     *  Anotação para ter o retorno da requisição para o client
+     *  Anotação PostMapping para informar que metodo é um post
+     *  Anotação ResponseStatus(HttpStatus.CREATED) retorno de msg pro client
      *  Anotação RequestBody indica que o objeto JSON será passado no corpo da requisição
      *  Anotação Valid para validar o objeto associado
-     *  Anotação ResponseStatus(HttpStatus.CREATED) retorno de msg pro client
      *
      */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Associado salvar( @RequestBody @Valid Associado associado ){
-        return repository.save(associado);
+        try {
+          return  service.salvar( associado );
+
+        }catch ( AssociadoNaoEncontadoException e){
+            throw new ResponseStatusException( HttpStatus.NOT_FOUND, e.getMessage() );
+        }
+
     }
 
     /**
      *  Anotação GetMapping para pegar o parâmeto passado na url
-     *  findById primeiro busca o associado
-     *  orElseThrow se não encontrar o cliente por id será lançado o erro que objeto não foi encontrado
      */
     @GetMapping("{id}")
     public Associado acharPorId( @PathVariable Integer id){
-        return repository
-                .findById(id)
-                .orElseThrow( ()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"associado não encontrado!"));
+        try {
+            return service.acharPorId(id);
 
+        }catch ( AssociadoNaoEncontadoException e ){
+            throw new ResponseStatusException( HttpStatus.NOT_FOUND, e.getMessage() );
+        }
     }
 
     /**
      *  Anotação DeleteMapping para pegar o parâmeto passado na url, informar que metodo é um Delete
      *  Anotação ResponseStatus(HttpStatus.NO_CONTENT) para ter o retorno 204 sucesso no NO_CONTENT
-     *  findById primeiro busca o associado
-     *  orElseThrow se não encontrar o cliente por id será lançado o erro que objeto não foi encontrado
      */
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletar( @PathVariable Integer id){
-        repository
-                .findById(id)
-                .map( associado -> {
-                    repository.delete(associado);
-                    return Void.TYPE;
-                })
-                .orElseThrow( ()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"associado não encontrado!"));
+        try {
+            service.deletar(id);
+        }catch ( AssociadoNaoEncontadoException e ){
+            throw new ResponseStatusException( HttpStatus.NOT_FOUND, e.getMessage() );
+        }
     }
 
     /**
      *  Anotação PutMapping para pegar o parâmeto passado na url, informar que metodo é um Put(update)
      *  Anotação ResponseStatus(HttpStatus.NO_CONTENT) para ter o retorno 204 sucesso no NO_CONTENT
-     *  findById primeiro busca o associado
-     *  orElseThrow se não encontrar o cliente por id será lançado o erro que objeto não foi encontrado
      */
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void atualizar( @PathVariable Integer id, @RequestBody @Valid Associado associadoAtualizado){
-        repository
-                .findById(id)
-                .map( associado -> {
-                    associadoAtualizado.setId(associado.getId());
-                    return repository.save(associadoAtualizado);
-                })
-                .orElseThrow( ()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"associado não encontrado!"));
+        try {
+            service.atualizar(id, associadoAtualizado);
+        }catch ( AssociadoNaoEncontadoException e ){
+            throw new ResponseStatusException( HttpStatus.NOT_FOUND, e.getMessage() );
+        }
+
     }
 
 }
