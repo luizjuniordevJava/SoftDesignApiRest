70e9c61b20be114bd65561e327e55f9bffe7a3e0
Luiz Junior <lsjunior.it@gmail.com>
2021-02-21 23:21:01 -0300

    Implementação da Classe AssociadoDTO.java

--- src/main/java/br/com/softdesign/service/dto/AssociadoDTO.java
+++ src/main/java/br/com/softdesign/service/dto/AssociadoDTO.java
@@ -4,13 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class AssociadoDTO {
+    @NotEmpty(message = "{campo.nome.obrigatorio}")
     private String nome;
+    @NotNull(message = "{campo.cpf.obrigatorio}")
+    @CPF(message = "{campo.cpf.invalido}")
     private String cpf;
     private String usuario;
 
